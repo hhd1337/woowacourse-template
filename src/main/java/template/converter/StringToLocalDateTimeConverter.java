f4d7ebb461b8fd7601280ae386139b9bb14e1cc2
goeka <goeka1337@gmail.com>
2026-01-03 18:26:04 +0900

    feat: 문자열 LocalTime 으로 바꿔 반환하는 컨버터 메소드 작성

--- src/main/java/template/converter/StringToLocalDateTimeConverter.java
+++ src/main/java/template/converter/StringToLocalDateTimeConverter.java
@@ -12,7 +12,9 @@ public class StringToLocalDateTimeConverter implements Converter<String, LocalDa
     private static final DateTimeFormatter HOUR_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH");
     private static final DateTimeFormatter MINUTE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
     private static final DateTimeFormatter SECOND_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-    private static final DateTimeFormatter TimeSecond = DateTimeFormatter.ofPattern("HH:mm:ss");
+    private static final DateTimeFormatter TIME_SECOND_FORMATTER = DateTimeFormatter.ofPattern("HH:mm:ss");
+    private static final DateTimeFormatter TIME_MINUTE_FORMATTER = DateTimeFormatter.ofPattern("HH:mm");
+
     // 만약 /로 구분자가 되어있다면 yyyy-MM-dd HH:mm:ss 를 yyyy/MM/dd HH:mm:ss 로 변경!!
 
     @Override
@@ -60,13 +62,21 @@ public class StringToLocalDateTimeConverter implements Converter<String, LocalDa
     public LocalTime convertToTimeSecond(String value) {
         String trimmed = validateAndTrim(value);
         try {
-            return LocalTime.parse(trimmed, TimeSecond);
+            return LocalTime.parse(trimmed, TIME_SECOND_FORMATTER);
         } catch (DateTimeParseException e) {
             throw new IllegalArgumentException("시간(초) 형식이 올바르지 않습니다. 예) 09:30:15");
         }
     }
 
-
+    public LocalTime convertToTimeMinute(String value) {
+        String trimmed = validateAndTrim(value);
+        try {
+            return LocalTime.parse(trimmed, TIME_MINUTE_FORMATTER);
+        } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException("시간(분) 형식이 올바르지 않습니다. 예) 09:59");
+        }
+    }
+    
     private String validateAndTrim(String value) {
         if (value == null || value.isBlank()) {
             throw new IllegalArgumentException("날짜/시간 입력이 비어있습니다.");
