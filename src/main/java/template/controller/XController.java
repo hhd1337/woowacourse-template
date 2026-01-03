9028ca89035198b886c1cc255f3b1379b4c99728
goeka <goeka1337@gmail.com>
2026-01-03 23:00:17 +0900

    feat: 메뉴, 컨트롤러에 메뉴 사용 부분 템플릿화

--- src/main/java/template/controller/XController.java
+++ src/main/java/template/controller/XController.java
@@ -13,6 +13,28 @@ public class XController {
     }
 
     public void process() {
-
+        // 만약 메뉴가 있고 Q를 입력받기 전까지 반복되어야 한다면 추가 (1/2)
+//        Menu menu;
+//        do {
+//            outputView.printHelloAndMenu(DateTimes.now());
+//            menu = inputHandler.inputMenu();
+//            run(menu, crewCatalog, crewAttendances, DateTimes.now());
+//        } while (menu != Menu.QUIT);
     }
+
+    // 메뉴가 있고 Q를 입력받기 전까지 반복되어야 한다면 추가 (2/2)
+//    private void run(Menu menu) {
+//        if (menu == Menu.A) {
+//            // runA();
+//        }
+//        if (menu == Menu.B) {
+//            // runB();
+//        }
+//        if (menu == Menu.C) {
+//            // runC();
+//        }
+//        if (menu == Menu.D) {
+//            // runD();
+//        }
+//    }
 }
